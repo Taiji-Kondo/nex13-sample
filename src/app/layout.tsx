5f5b6f64f1b7bf2235142ec1090c4be653b24390
taiji <k.taiji1108@gmail.com>
2023-02-03 18:38:57 +0900

    chore: Simply

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
-import './globals.css'
+import {ReactNode} from "react";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="jp">
       <head />
       <body>{children}</body>
     </html>
