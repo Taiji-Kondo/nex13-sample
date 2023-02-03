ea52e7efda2a0f440f6644b75c2b1cfe04f87db2
taiji <k.taiji1108@gmail.com>
2023-02-03 21:35:18 +0900

    feat: Create guest page

---
+++ src/app/(guest)/guest/page.tsx
@@ -0,0 +1,11 @@
+const Guest = () => {
+  return (
+    <div className={'container mx-auto'}>
+      <h2 className={'font-bold text-md'}>
+        Guest Page
+      </h2>
+    </div>
+  )
+}
+
+export default Guest
---
+++ src/app/(guest)/layout.tsx
@@ -0,0 +1,22 @@
+import '@/app/styles/globals.css'
+
+import {ReactNode} from "react";
+import {BaseHeader} from "@/components/header/BaseHeader";
+import {HeaderNavigation} from "@/components/navigation/HeaderNavigation";
+
+const GuestLayout = ({
+  children,
+}: {
+  children: ReactNode
+}) => {
+  return (
+    <body>
+      <BaseHeader />
+      <main>
+        {children}
+      </main>
+    </body>
+  )
+}
+
+export default GuestLayout
