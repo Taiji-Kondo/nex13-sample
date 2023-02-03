2fe20ba2c03046135546c0d4f9a7bc29fc882ee5
taiji <k.taiji1108@gmail.com>
2023-02-03 21:34:28 +0900

    feat: Move top page for click header title

--- src/components/header/BaseHeader.tsx
+++ src/components/header/BaseHeader.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export const BaseHeader = () => {
   return (
     <header className={'flex items-center h-20 bg-black'}>
       <div className={'container mx-auto'}>
-        <h1 className={'text-white font-bold text-xl'}>Next13 Playground</h1>
+        <Link href={'/'}>
+          <h1 className={'text-white font-bold text-xl'}>Next13 Playground</h1>
+        </Link>
       </div>
     </header>
   )
