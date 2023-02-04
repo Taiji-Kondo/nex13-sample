c21922acbb768aaabfabe0d0ed4b6295ade928fc
taiji <k.taiji1108@gmail.com>
2023-02-04 17:19:27 +0900

    feat: Suspend PostList

--- src/app/(auth)/post/page.tsx
+++ src/app/(auth)/post/page.tsx
@@ -1,9 +1,17 @@
+import {Suspense} from "react";
+import {PostList} from "@/feature/post/PostList";
+
 const Post = () => {
   return (
     <div className={'container mx-auto'}>
       <h2 className={'font-bold text-md'}>
         Post Page
       </h2>
+
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <PostList />
+      </Suspense>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import '@/app/styles/globals.css'
 
 import {ReactNode} from "react";
-import {BaseHeader} from "@/components/header/BaseHeader";
-import {HeaderNavigation} from "@/components/navigation/HeaderNavigation";
 
 const RootLayout = ({
   children,
