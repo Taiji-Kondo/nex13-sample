d4e5168ba835ce89a2d7fdac08e243f44362c3c2
taiji <k.taiji1108@gmail.com>
2023-02-05 16:17:54 +0900

    feat: useTransition

--- src/app/(auth)/memory/page.tsx
+++ src/app/(auth)/memory/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {Suspense, useState} from "react";
+import {Suspense, useState, useTransition} from "react";
 import {Loader} from "@/components/loader/Loader";
 import {Photos} from "@/feature/memory/Photos";
 import {Albums} from "@/feature/memory/Albums";
@@ -13,7 +13,14 @@ const TAB_PANEL = {
 type TabPanelKeyType = keyof typeof TAB_PANEL
 
 const Memory = () => {
+  const [isPending, startTransition] = useTransition()
   const [currentTab, setCurrentTab] = useState<TabPanelKeyType>('photo')
+
+  const onClickTab = (tab: TabPanelKeyType) => {
+    startTransition(() => {
+      setCurrentTab(tab)
+    })
+  }
 
   return (
     <div className={'container mx-auto'}>
@@ -21,22 +28,24 @@ const Memory = () => {
         Memory Page
       </h2>
 
-      <Suspense fallback={<Loader />}>
-        <div className={'flex'}>
-          <button
-            className={`p-3 rounded-md text-white ${currentTab === 'photo' ? 'bg-gray-500' : 'bg-gray-300'}`}
-            onClick={() => setCurrentTab('photo')}
-          >
-            PHOTO
-          </button>
-          <button
-            className={`p-3 rounded-md text-white ${currentTab === 'album' ? 'bg-gray-500' : 'bg-gray-300'}`}
-            onClick={() => setCurrentTab('album')}
-          >
-            ALBUM
-          </button>
-        </div>
+      <div className={'flex'}>
+        <button
+          className={`p-3 rounded-md text-white ${currentTab === 'photo' ? 'bg-gray-500' : 'bg-gray-300'} ${isPending ? 'opacity-50' : ''}`}
+          onClick={() => onClickTab('photo')}
+          disabled={isPending}
+        >
+          PHOTO
+        </button>
+        <button
+          className={`p-3 rounded-md text-white ${currentTab === 'album' ? 'bg-gray-500' : 'bg-gray-300'} ${isPending ? 'opacity-50' : ''}`}
+          onClick={() => onClickTab('album')}
+          disabled={isPending}
+        >
+          ALBUM
+        </button>
+      </div>
 
+      <Suspense fallback={<Loader />}>
         <div className={'border-2 p-3'}>
           <h3 className={'text-md font-bold'}>
             {currentTab}
