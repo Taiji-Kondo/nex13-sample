079d2f1b21a57b4ad3962eb9695b220c003afd22
taiji <k.taiji1108@gmail.com>
2023-02-05 12:47:27 +0900

    feat: Create not-found page

---
+++ src/app/(auth)/post/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+  return (
+    <div className={'container mx-auto'}>
+      <h2 className={'font-bold text-md'}>
+        This Post Not Found
+      </h2>
+    </div>
+  )
+}
+
+export default NotFoundPage
--- src/app/(auth)/post/[id]/page.tsx
+++ src/app/(auth)/post/[id]/page.tsx
@@ -5,12 +5,19 @@ import {Post} from "@/feature/post/Post";
 import {Loadable} from "@/helpers/Loadable";
 import axios from "axios";
 import {PostType} from "@/types/model/PostType";
+import {notFound} from "next/navigation";
 
 const PostDetail = ({params}: {params: {id: number}}) => {
   const id = Number(params.id)
   const getPost = async () => {
-    const response = await axios.get<PostType>(`https://jsonplaceholder.typicode.com/posts/${id}`)
-    return response.data
+    try {
+      const response = await axios.get<PostType>(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      if (!response.data) throw new Error(`post ${id} is not found`)
+      return response.data
+    } catch (error) {
+      console.error(error)
+      notFound()
+    }
   }
   const [data] = useState(() => new Loadable(getPost()));
 
