e46d9347cf1c9567735ad846811f534de443d31a
taiji <k.taiji1108@gmail.com>
2023-02-05 16:17:34 +0900

    chore: Wait for 3000ms in getAlbums

--- src/feature/memory/api/getAlbums.ts
+++ src/feature/memory/api/getAlbums.ts
@@ -4,7 +4,10 @@ import {AlbumType} from "@/types/model/AlbumType";
 let albums: AlbumType[] | undefined = undefined
 export const getAlbums = () => {
   if (albums === undefined) {
-    throw axios.get<AlbumType[]>('https://jsonplaceholder.typicode.com/albums').then((response) => albums = response.data)
+    throw new Promise((resolve) => setTimeout(resolve, 3000)).then(() => {
+      return axios.get<AlbumType[]>('https://jsonplaceholder.typicode.com/albums').then((response) => albums = response.data)
+    });
+    // throw axios.get<AlbumType[]>('https://jsonplaceholder.typicode.com/albums').then((response) => albums = response.data)
   }
   return albums
 }
