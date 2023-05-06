700cf1e3ccd631ffdab2daa70a0da89212b5b3fb
taiji <k.taiji1108@gmail.com>
2023-05-06 15:53:21 +0900

    feat: Update Next.js 13.4

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-    // ↓ subset font ↓
-    fontLoaders: [
-      { loader: '@next/font/google', options: { subsets: ['latin'] } },
-    ],
-  },
   async rewrites() {
     return [
       {
